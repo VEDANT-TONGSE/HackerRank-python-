8e5f0d1f56616b0b1dbfe8c9e086bf73ae4aa344
VEDANT ASHOK TONGSE <vedant.tongse.vt@gmail.com>
2026-01-01 01:51:16 +0530

    hackerrank Dot and Cross

---
+++ hackerrank Dot and Cross.py
@@ -0,0 +1,33 @@
+""" hackerrank Dot and Cross 
+Task
+You are given two arrays A and B. Both have dimensions of N X N.
+Your task is to compute their matrix product.
+
+Input Format
+The first line contains the integer N.
+The next N lines contains N space separated integers of array A.
+The following N lines contains N space separated integers of array B.
+
+
+Output Format
+Print the matrix multiplication of A and B.
+
+Sample Input
+
+2
+1 2
+3 4
+1 2
+3 4
+Sample Output
+
+[[ 7 10]
+ [15 22]]
+"""
+
+import numpy as np
+
+n = int(input())
+a = np.array([input().split() for _ in range(n)], int)
+b = np.array([input().split() for _ in range(n)], int)
+print(np.dot(a, b))
